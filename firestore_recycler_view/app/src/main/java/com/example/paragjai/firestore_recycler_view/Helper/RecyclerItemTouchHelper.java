a6786160f13a9bbf87356f63331e6b295c494cef
Jain <parag.jain@intel.com>
2018-09-10 11:07:04 +0530

    Able to retrieve stops for selected route from firestore. Displays in recyclerview/cardview.

---
+++ firestore_recycler_view/app/src/main/java/com/example/paragjai/firestore_recycler_view/Helper/RecyclerItemTouchHelper.java
@@ -0,0 +1,66 @@
+package com.example.paragjai.firestore_recycler_view.Helper;
+
+import android.graphics.Canvas;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.View;
+
+import com.example.paragjai.firestore_recycler_view.MyRecyclerViewAdapterForStops;
+import com.example.paragjai.firestore_recycler_view.MyRecyclerViewHolderForStops;
+
+public class RecyclerItemTouchHelper extends ItemTouchHelper.SimpleCallback {
+
+    private RecyclerItemTouchHelperListener listener;
+
+    public RecyclerItemTouchHelper(int dragDirs, int swipeDirs, RecyclerItemTouchHelperListener listener){
+        super(dragDirs, swipeDirs);
+        this.listener = listener;
+    }
+
+    @Override
+    public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder viewHolder1) {
+        return true;
+    }
+
+    @Override
+    public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+
+        if(listener!=null)
+        {
+            listener.onSwiped(viewHolder, direction, viewHolder.getAdapterPosition());
+        }
+    }
+
+    @Override
+    public void onSelectedChanged(@Nullable RecyclerView.ViewHolder viewHolder, int actionState)
+    {
+        if(viewHolder!=null)
+        {
+            View foregroundView = ((MyRecyclerViewHolderForStops) viewHolder).mViewForeground;
+            getDefaultUIUtil().onSelected(foregroundView);
+        }
+    }
+
+    @Override
+    public void clearView(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder)
+    {
+        View foregroundView = ((MyRecyclerViewHolderForStops) viewHolder).mViewForeground;
+        getDefaultUIUtil().clearView(foregroundView);
+    }
+
+    @Override
+    public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive)
+    {
+        View foregroundView = ((MyRecyclerViewHolderForStops) viewHolder).mViewForeground;
+        getDefaultUIUtil().onDraw(c, recyclerView, foregroundView, dX, dY, actionState, isCurrentlyActive);
+    }
+
+    @Override
+    public void onChildDrawOver(@NonNull Canvas c, @NonNull RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive)
+    {
+        View foregroundView = ((MyRecyclerViewHolderForStops) viewHolder).mViewForeground;
+        getDefaultUIUtil().onDrawOver(c, recyclerView, foregroundView, dX, dY, actionState, isCurrentlyActive);
+    }
+}
---
+++ firestore_recycler_view/app/src/main/java/com/example/paragjai/firestore_recycler_view/HttpGetRequest.java
@@ -0,0 +1,72 @@
+package com.example.paragjai.firestore_recycler_view;
+
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/* A complete post for calling a URL from Android */
+
+public class HttpGetRequest extends AsyncTask<String, Void, String> {
+
+    public static final String REQUEST_METHOD = "GET";
+    public static final int READ_TIMEOUT = 15000;
+    public static final int CONNECTION_TIMEOUT = 15000;
+    String inputLine;
+    String response;
+
+    @Override
+    protected String doInBackground(String... strings) {
+        String requiredURL = strings[0];
+        try{
+            //Create a URL object holding our URL
+            URL myURL = new URL(requiredURL);
+
+            //Create a connection
+            HttpURLConnection connection = (HttpURLConnection) myURL.openConnection();
+
+            //Set methods and timeout
+            connection.setRequestMethod(REQUEST_METHOD);
+            connection.setReadTimeout(READ_TIMEOUT);
+            connection.setConnectTimeout(CONNECTION_TIMEOUT);
+
+            //Connect to the URL
+            connection.connect();
+
+            /* Getting the response */
+
+            //Create a new InputStreamReader
+            InputStreamReader streamReader = new InputStreamReader(connection.getInputStream());
+
+            //Create a new buffered reader and String Builder
+            BufferedReader reader = new BufferedReader(streamReader);
+            StringBuilder stringBuilder = new StringBuilder();
+
+            //Check if the line we are reading is not null
+            while((inputLine = reader.readLine()) != null){
+                stringBuilder.append(inputLine);
+            }
+
+            //Close our InputStream and Buffered reader
+            reader.close();
+            streamReader.close();
+            //Set our result equal to our stringBuilder
+            response = stringBuilder.toString();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            response = null;
+        }
+        return response;
+    }
+
+
+    @Override
+    protected void onPostExecute(String result) {
+        super.onPostExecute(result);
+    }
+}
